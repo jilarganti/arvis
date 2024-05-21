d82363c29d813911ba66229026983b2bf32d0ba8
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2024-05-21 13:54:03 +0400

    release: v0.0.2

---
+++ packages/arvis/CHANGELOG.md
@@ -0,0 +1,4 @@
+## [0.0.2](https://github.com/jilarganti/arvis/compare/0.1.18...0.0.2) (2024-05-21)
+
+
+
---
+++ scripts/release.ts
@@ -0,0 +1,28 @@
+import { release } from '@vitejs/release-scripts'
+import colors from 'picocolors'
+import { logRecentCommits, run, updateTemplateVersions } from './releaseUtils'
+
+release({
+  repo: 'arvis',
+  packages: ['arvis', 'create-vite', 'plugin-legacy'],
+  toTag: (pkg, version) =>
+    pkg === 'arvis' ? `v${version}` : `${pkg}@${version}`,
+  logChangelog: (pkg) => logRecentCommits(pkg),
+  generateChangelog: async (pkgName) => {
+    if (pkgName === 'create-vite') await updateTemplateVersions()
+
+    console.log(colors.cyan('\nGenerating changelog...'))
+    const changelogArgs = [
+      'conventional-changelog',
+      '-p',
+      'angular',
+      '-i',
+      'CHANGELOG.md',
+      '-s',
+      '--commit-path',
+      '.',
+    ]
+    if (pkgName !== 'arvis') changelogArgs.push('--lerna-package', pkgName)
+    await run('npx', changelogArgs, { cwd: `packages/${pkgName}` })
+  },
+})
