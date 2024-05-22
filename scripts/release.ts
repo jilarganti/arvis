ccbf6ead94a6730f48dfba80ed3feed9f9881b0f
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2024-05-22 18:35:56 +0400

    release: docs@0.1.18

--- scripts/release.ts
+++ scripts/release.ts
@@ -7,6 +7,7 @@ let path = 'packages/'
 release({
     repo: 'arvis',
     packages: ['arvis', 'create-arvis', 'docs', 'ai', 'media'],
+    getPkgDir: (pkg) => (pkg === 'docs' ? 'docs' : `packages/${pkg}`),
     toTag: (pkg, version) => (pkg === 'arvis' ? `v${version}` : `${pkg}@${version}`),
     logChangelog: (pkg) => logRecentCommits(pkg),
     generateChangelog: async (pkgName) => {
