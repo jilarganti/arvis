cf8e942242276bccb8eab598445c48b6cc610b6b
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2024-05-22 19:16:35 +0400

    chore: Update package version to 1.0.2

--- packages/arvis/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-## [0.0.2](https://github.com/jilarganti/arvis/compare/0.1.18...0.0.2) (2024-05-21)
-
-
-
--- scripts/release.ts
+++ scripts/release.ts
@@ -7,11 +7,12 @@ let path = 'packages/'
 release({
     repo: 'arvis',
     packages: ['arvis', 'create-arvis', 'docs', 'ai', 'media'],
-    getPkgDir: (pkg) => (pkg === 'docs' ? 'docs' : `packages/${pkg}`),
+    // getPkgDir: (pkg) => (pkg === 'docs' ? 'docs' : `packages/${pkg}`),
     toTag: (pkg, version) => (pkg === 'arvis' ? `v${version}` : `${pkg}@${version}`),
     logChangelog: (pkg) => logRecentCommits(pkg),
     generateChangelog: async (pkgName) => {
         if (pkgName === 'create-arvis') await updateTemplateVersions()
+
         if (pkgName === 'docs') {
             path = ''
             await updateDocsVersion()
