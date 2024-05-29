cc85657e9dd486ab89fced2f6e510a0c95120d24
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2024-05-29 19:56:40 +0400

    fix: Update dependencies and remove unused files

--- scripts/publishCI.ts
+++
@@ -1,8 +0,0 @@
-import { publish } from '@vitejs/release-scripts'
-
-// Check the tag passed in CI, and skip provenance if tag has `@` due to
-// https://github.com/slsa-framework/slsa-github-generator/pull/2758 not released
-const tag = process.argv.slice(2)[0] ?? ''
-const provenance = !tag.includes('@')
-
-publish({ defaultPackage: 'arvis', provenance, packageManager: 'pnpm' })
