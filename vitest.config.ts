2b45872abf8b72c047c7be41bd0e7c36ed8a99d0
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2024-06-14 10:18:05 +0400

    ```text chore: Update vitest.config.ts to include new aliases
    This commit updates the vitest.config.ts file to include new aliases for '@arvis' and '@utils' in the resolve configuration. This ensures that the correct paths are resolved when importing modules using these aliases.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,20 @@
-import { defaultExclude, defineConfig } from 'vitest/config'
-import { alias } from './alias'
+import { resolve } from 'node:path'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     optimizeDeps: {
         entries: [],
     },
     resolve: {
-        alias,
+        alias: {
+            '@arvis': resolve(__dirname, './apps/a.r.v.i.s./src/'),
+            '@utils': resolve(__dirname, './packages/utils/src/'),
+        },
     },
     test: {
         testTimeout: 30_000,
         name: 'unit',
         setupFiles: ['./test/setup.ts'],
         include: ['test/**/*.{test,spec}.{ts,js}'],
-        // exclude: [...defaultExclude, '**/site/**'],
     },
 })
