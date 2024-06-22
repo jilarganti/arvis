66e0f13a6854c2a45eb13e840cc95ee750ab8493
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2024-06-22 15:06:53 +0400

    chore: Update TypeScript ESLint configuration and dependencies

--- config/eslint.config.js
+++ config/eslint.config.js
@@ -1,15 +1,20 @@
-import globals from 'globals'
+// @ts-check
+
+/**
+ * @see https://typescript-eslint.io/getting-started/
+ */
 import tseslint from 'typescript-eslint'
 
-export default [
+export default tseslint.config(
+    ...tseslint.configs.strict,
+    ...tseslint.configs.stylistic,
+
     {
         ignores: ['**/dist'],
     },
-    { languageOptions: { globals: globals.browser } },
-    ...tseslint.configs.recommended,
     {
         rules: {
             '@typescript-eslint/no-unused-vars': 'warn',
         },
     },
-]
+)
