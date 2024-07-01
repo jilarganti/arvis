4604acc3b8d30a2f4087a348561d9bdd41a9ed96
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2024-07-01 14:31:08 +0400

    chore: Update playwright.config.ts to remove reuseExistingServer option and modify trace option

--- configs/playwright.config.ts
+++ configs/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from "@playwright/test"
 
 const baseUrl = process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:3000/"
-console.log("baseUrl", baseUrl)
+// console.log("baseUrl", baseUrl)
 
 /**
  * @see https://playwright.dev/docs/test-configuration.
