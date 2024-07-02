c713a978275f11e789ae1f6f4bd30f477eac10ae
AlexeyKorolev <128805676+aleksey-korolyov@users.noreply.github.com>
2024-07-02 08:32:39 +0400

    chore: Update test-1.test.ts to navigate to the introduction page and interact with theme switches

--- packages/site/e2e/test-1.test.ts
+++ packages/site/e2e/test-1.test.ts
@@ -2,9 +2,9 @@ import { test, expect } from "@playwright/test"
 
 test("test", async ({ page }) => {
   await page.goto("/")
-  // await page.goto("/guide/introduction#what-is-a-r-v-i-s")
-  // await page.getByRole("switch", { name: "Switch to dark theme" }).click()
-  // await page.getByRole("switch", { name: "Switch to light theme" }).click()
+  await page.goto("/guide/introduction#what-is-a-r-v-i-s")
+  await page.getByRole("switch", { name: "Switch to dark theme" }).click()
+  await page.getByRole("switch", { name: "Switch to light theme" }).click()
   // const page1Promise = page.waitForEvent("popup")
   // await page.getByRole("link", { name: "github" }).click()
   // const page1 = await page1Promise
